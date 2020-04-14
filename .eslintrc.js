bd3682e74b7b2ecbadbe1d445551773a7497e50d
Vasanth Developer <vasanth@vasanthdeveloper.com>
2020-04-14 11:13:23 +0530

    💥 Boom!

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'es6': true,
+        'node': true
+    },
+    'extends': [
+        'plugin:prettier/recommended',
+        'eslint:recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+    ],
+    'globals': {
+        'Atomics': 'readonly',
+        'SharedArrayBuffer': 'readonly'
+    },
+    'parser': '@typescript-eslint/parser',
+    'parserOptions': {
+        'ecmaVersion': 2018,
+        'sourceType': 'module'
+    },
+    'plugins': [
+        '@typescript-eslint'
+    ],
+    'rules': {
+        'indent': [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'never'
+        ]
+    }
+}
---
+++ src/laghu.ts
@@ -0,0 +1,12 @@
+//   ___    __________   |  Vasanth Developer (Vasanth Srivatsa)
+//   __ |  / /___  __ \  |  ------------------------------------------------
+//   __ | / / __  / / /  |  https://github.com/vasanthdeveloper/laghu.git
+//   __ |/ /  _  /_/ /   |
+//   _____/   /_____/    |  Entryfile for laghu file
+//                       |
+
+async function main(): Promise<void> {
+    console.log('hi! :)')
+}
+
+main()
